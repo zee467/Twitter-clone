767e8473cf7909ccf2c182231538aaf19fcb3fc2
zee467 <wura.mahmud@gmail.com>
2023-04-26 02:46:13 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+# instantiate flask app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
