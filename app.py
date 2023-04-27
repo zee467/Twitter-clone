1cb55514ab70eb8caf1b7bb212c5a6c36cd356ce
zee467 <wura.mahmud@gmail.com>
2023-04-27 05:49:46 +0100

    Added User model with fields

--- app.py
+++ app.py
@@ -5,16 +5,24 @@ import os
 
 # instantiate flask app
 app = Flask(__name__)
-# configure the database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# configure the database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
 # initialize the app with the extension
 db = SQLAlchemy(app)
-migrate = Migrate(app)
+migrate = Migrate(app, db)
+
+# Create user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(30))
+    image = db.Column(db.String(100))
+    password = db.Column(db.String(50))
 
 
 @app.route("/")
-def index():
+def index(): 
     return render_template("index.html")
 
 @app.route("/profile")
