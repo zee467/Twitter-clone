7ef5a131fb574cc383d8837910d03829471a4830
zee467 <wura.mahmud@gmail.com>
2023-05-05 13:30:41 +0100

    Implement profile information display

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import cloudinary
 from cloudinary.uploader import upload
 from dotenv import load_dotenv
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+from datetime import datetime
 import os
 
 # instantiate flask app
@@ -32,6 +33,7 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 # instantiate login manager
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # tells flask-login where to redirect the user if they are trying to access an unauthorized page
 
 
 # initialize the app with the extension
@@ -45,10 +47,12 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(30))
     image = db.Column(db.String(100))
     password = db.Column(db.String(50))
+    join_date = db.Column(db.DateTime)
 
 
 @login_manager.user_loader
 def load_user(user_id):
+    # Logic to load a user from the database based on user_id
     return User.query.get(int(user_id))
 
 
@@ -92,8 +96,20 @@ def login():
     return render_template("login.html", form=form)
 
 @app.route("/profile")
+@login_required
 def profile():
-    return render_template("profile.html")
+    # Get the current user's file path from the database
+    file_path = current_user.image 
+
+    # Generate the Cloudinary URL for the file
+    file_url = cloudinary.CloudinaryImage(file_path).build_url()
+    return render_template("profile.html", current_user=current_user, file_url=file_url)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index.html'))
 
 @app.route("/timeline")
 def timeline():
@@ -113,8 +129,10 @@ def register():
             
             # Upload the file to Cloudinary
             upload_result = upload(file_to_upload, public_id=file_path)
+            print(upload_result)
 
-        new_user = User(name=form.name.data, username=form.username.data, password=generate_password_hash(form.password.data))
+        new_user = User(name=form.name.data, username=form.username.data, image=upload_result['secure_url'], \
+                        password=generate_password_hash(form.password.data), join_date=datetime.now())
         
         db.session.add(new_user)
         db.session.commit()
